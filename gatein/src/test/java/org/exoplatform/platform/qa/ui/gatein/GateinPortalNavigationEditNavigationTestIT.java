26cf1dde4b5bc7c5c106241f13ccdbb79277eee9
ilyeskallala <ikallala@exoplatform.com>
2017-08-02 15:19:55 +0100

    split gatein tests

--- gatein/src/test/java/org/exoplatform/platform/qa/ui/gatein/GateinPortalNavigationEditNavigationTestIT.java
+++ gatein/src/test/java/org/exoplatform/platform/qa/ui/gatein/GateinPortalNavigationEditNavigationTestIT.java
@@ -66,6 +66,7 @@ public class GateinPortalNavigationEditNavigationTestIT extends Base {
      * node, except the Node Name. - Click Save Input Data: Expected Outcome:
      * The node is updated with the change value
      */
+
     manageLogInOut.signIn("root","gtn");
     navigationToolbar.goToPotalSites();
     portalmanagesites.goToEditNavigation();
@@ -160,7 +161,168 @@ public class GateinPortalNavigationEditNavigationTestIT extends Base {
     pagecreationwizard.saveChangesPageEditor();
     if (!titleActual.equals(newTitlePage))
       assert false : "The title:" + newTitlePage + " is not updated";
+    $(ELEMENT_NAVIGATION_MANAGEMENT_SAVE).click();
+    navigationToolbar.goToPotalSites();
+    portalmanagesites.goToEditNavigation();
 
+    info("Test 03: Delete node");
+    /*
+     * Step Number: 1 Step Name: Step 1: Delete Node Step Description: - Go to
+     * Administration/Portal/ Sites/Edit navigation - Select a node and choose
+     * Delete node by right click - Click OK in the confirmation message to
+     * accept your deletion. - Click Save Input Data: Expected Outcome: - The
+     * node is removed from the list
+     */
+    navigationmanagement.deleteNode(nodeName);
+
+  }
+
+  /**
+   * *
+   * <li>Case ID:123122.</li>
+   * <li>Test Case Name:Delete a node.</li>
+   * <li>Case ID:123123.</li>
+   * <li>Test Case Name:Add node for portal.</li>
+   * <li>Case ID:123041.</li>
+   * <li>Test Case Name:Edit node's page properties.</li>
+   */
+  @Test
+  public void test02_Add_Edit_Delete_NodePage() {
+    info("Test 01: Add new node");
+    String portalName = "portalName" + getRandomNumber();
+    String nodeName = "nodeName" + getRandomNumber();
+    String namePage = "namePage" + getRandomNumber();
+    String titlePage = "titlePage" + getRandomNumber();
+
+    String newTitlePage = "newTitlePage" + getRandomNumber();
+    String groupPath = "Development";
+    String memberships = "author";
+    /*
+     * Step Number: 1 Step Name: Step 1: Add node for portal Step Description: -
+     * Go to Administration/Portal/ Sites/Edit navigation - Add new node by
+     * click add new node button or right click - Input value for Page Node
+     * Setting and Page Selector form - Click Save Input Data: Expected Outcome:
+     * Add node successfully
+     */
+    manageLogInOut.signIn("root","gtn");
+    navigationToolbar.goToPotalSites();
+    portalmanagesites.goToEditNavigation();
+    info("Add a new node");
+    navigationmanagement.addNode(nodeName, "");
+    navigationmanagement.inputInfoPageSelector(namePage, titlePage, true, false, false);
+    navigationmanagement.saveNode();
+
+    info("Test 02: Edit node Page");
+    /*
+     * Step Number: 1 Step Name: Step 1: Edit node's page properties Step
+     * Description: - Go to Administration/Portal/Sites/Edit navigation - Select
+     * a node - Right click and choose Edit node's page - Choose View Page
+     * properties on Page Editor - Edit some changes - Click Save button Input
+     * Data: Expected Outcome: Page Setting, Permission setting tab are updated
+     * successfully with new changes
+     */
+    portalmanagesites.goToEditNavigation();
+    navigationmanagement.editNodePage(nodeName);// go to declaration de automate
+    // right click
+    pagecreationwizard.viewProperties();
+    pagecreationwizard.changeProperties(newTitlePage, groupPath, memberships, true, false);
+
+    info("Verify that the changs are updated");
+    pagecreationwizard.viewProperties();
+    info("Page settings is updated");
+    String titleActual = pagecreationwizard.getOldTitle();
+    info("titleActual:" + titleActual);
+    info("newtitle:" + newTitlePage);
+    info("Permission setting tab is updated");
+    click(ELEMENT_VIEW_PROPERTIES_PERMISSION_TAB);
+
+    $(byText("/developers")).waitUntil(Condition.appears, 10000);
+    pagecreationwizard.saveChangeProperties();
+    pagecreationwizard.saveChangesPageEditor();
+    if (!titleActual.equals(newTitlePage))
+      assert false : "The title:" + newTitlePage + " is not updated";
+    $(ELEMENT_NAVIGATION_MANAGEMENT_SAVE).click();
+    navigationToolbar.goToPotalSites();
+    portalmanagesites.goToEditNavigation();
+    info("Test 03: Delete node");
+    /*
+     * Step Number: 1 Step Name: Step 1: Delete Node Step Description: - Go to
+     * Administration/Portal/ Sites/Edit navigation - Select a node and choose
+     * Delete node by right click - Click OK in the confirmation message to
+     * accept your deletion. - Click Save Input Data: Expected Outcome: - The
+     * node is removed from the list
+     */
+    navigationmanagement.deleteNode(nodeName);
+
+  }
+
+  /**
+   * *
+   * <li>Case ID:123122.</li>
+   * <li>Test Case Name:Delete a node.</li>
+   * <li>Case ID:123123.</li>
+   * <li>Test Case Name:Add node for portal.</li>
+   * <li>Case ID:123041.</li>
+   * <li>Test Case Name:Edit node's page properties.</li>
+   */
+  @Test
+  public void test03_Add_Edit_Delete_NodePage() {
+    info("Test 01: Add new node");
+    String portalName = "portalName" + getRandomNumber();
+    String nodeName = "nodeName" + getRandomNumber();
+    String namePage = "namePage" + getRandomNumber();
+    String titlePage = "titlePage" + getRandomNumber();
+
+    String newTitlePage = "newTitlePage" + getRandomNumber();
+    String groupPath = "Development";
+    String memberships = "author";
+    /*
+     * Step Number: 1 Step Name: Step 1: Add node for portal Step Description: -
+     * Go to Administration/Portal/ Sites/Edit navigation - Add new node by
+     * click add new node button or right click - Input value for Page Node
+     * Setting and Page Selector form - Click Save Input Data: Expected Outcome:
+     * Add node successfully
+     */
+    manageLogInOut.signIn("root","gtn");
+    navigationToolbar.goToPotalSites();
+    portalmanagesites.goToEditNavigation();
+    info("Add a new node");
+    navigationmanagement.addNode(nodeName, "");
+    navigationmanagement.inputInfoPageSelector(namePage, titlePage, true, false, false);
+    navigationmanagement.saveNode();
+
+    info("Test 02: Edit node Page");
+    /*
+     * Step Number: 1 Step Name: Step 1: Edit node's page properties Step
+     * Description: - Go to Administration/Portal/Sites/Edit navigation - Select
+     * a node - Right click and choose Edit node's page - Choose View Page
+     * properties on Page Editor - Edit some changes - Click Save button Input
+     * Data: Expected Outcome: Page Setting, Permission setting tab are updated
+     * successfully with new changes
+     */
+    portalmanagesites.goToEditNavigation();
+    navigationmanagement.editNodePage(nodeName);// go to declaration de automate
+    // right click
+    pagecreationwizard.viewProperties();
+    pagecreationwizard.changeProperties(newTitlePage, groupPath, memberships, true, false);
+
+    info("Verify that the changs are updated");
+    pagecreationwizard.viewProperties();
+    info("Page settings is updated");
+    String titleActual = pagecreationwizard.getOldTitle();
+    info("titleActual:" + titleActual);
+    info("newtitle:" + newTitlePage);
+    info("Permission setting tab is updated");
+    click(ELEMENT_VIEW_PROPERTIES_PERMISSION_TAB);
+
+    $(byText("/developers")).waitUntil(Condition.appears, 10000);
+    pagecreationwizard.saveChangeProperties();
+    pagecreationwizard.saveChangesPageEditor();
+    if (!titleActual.equals(newTitlePage))
+      assert false : "The title:" + newTitlePage + " is not updated";
+    $(ELEMENT_NAVIGATION_MANAGEMENT_SAVE).click();
+    navigationToolbar.goToPotalSites();
+    portalmanagesites.goToEditNavigation();
     info("Test 03: Delete node");
     /*
      * Step Number: 1 Step Name: Step 1: Delete Node Step Description: - Go to
